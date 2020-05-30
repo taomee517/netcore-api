4950c62bb16da437ba0134bcc32ce4ba756730f0
Administrator <taomee517@gmail.com>
2020-05-30 17:04:43 +0800

    Initial commit

---
+++ netcore-api/Controllers/HealthCheckController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace netcore_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+       
+
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Console.WriteLine($"收到Consul健康检查请求！ time: {DateTime.Now}");
+            return Content("OK");
+        }
+    }
+}
---
+++ netcore-api/Controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace netcore_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+       
+
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello,DotNet Core Api";
+        }
+    }
+}
